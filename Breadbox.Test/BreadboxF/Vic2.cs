21c55bb0c17421da5c31db792e30e95253b5cbc7
Anthony Konzel <saxxonpike@gmail.com>
2016-05-15 21:20:23 -0500

    F# foundation.

---
+++ Breadbox.Test/BreadboxF/Vic2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BreadboxF;
+using NUnit.Framework;
+
+namespace Breadbox.Test.BreadboxF
+{
+    [TestFixture]
+    public class Vic2
+    {
+        [Test]
+        public void Test1()
+        {
+            var v2 = new Vic2Configuration(1, 2, 3, 4, 5, 6);
+        }
+    }
+}
