16c10b9e8d1620a4f64085baeeb8e53a8a9ef597
Anthony Konzel <saxxonpike@gmail.com>
2016-05-22 14:19:32 -0500

    Improve control register tests.

--- Breadbox.Test/Vic2/Vic2RegisterTests.cs
+++ Breadbox.Test/Vic2/Vic2RegisterTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using BreadboxF;
 using FluentAssertions;
 using NUnit.Framework;
@@ -15,6 +16,30 @@ namespace Breadbox.Test.Vic2
                 // config doesn't matter for these; we're using new NTSC here
                 return new CommodoreVic2Configuration(13, 65, 263);
             }
+        }
+
+        private void TestSpriteRegister(int register, int value, Func<int, bool> test)
+        {
+            // Arrange
+            var expectedValue = value & 0xFF;
+            var expectedBits = Enumerable.Range(0, 8).Select(i => (value & (1 << i)) != 0).ToArray();
+
+            // Act
+            Vic.WriteRegister(register, value);
+
+            // Assert
+            Vic.ReadRegister(register).Should().Be(expectedValue);
+            var observedBits = Enumerable.Range(0, 8).Select(test);
+            observedBits.ShouldAllBeEquivalentTo(expectedBits);
+        }
+
+        private void TestColorRegister(int register, int value)
+        {
+            // Act
+            Vic.WriteRegister(register, value);
+
+            // Assert
+            Vic.ReadRegister(register).Should().Be(0xF0 | (value & 0xF));
         }
 
         [Test]
@@ -77,6 +102,11 @@ namespace Breadbox.Test.Vic2
             // Assert
             var observedValue = Vic.ReadRegister(register);
             observedValue.Should().Be(expectedValue);
+            Vic.ExtraColorMode.Should().Be((value & 0x40) != 0);
+            Vic.BitmapMode.Should().Be((value & 0x20) != 0);
+            Vic.DisplayEnabled.Should().Be((value & 0x10) != 0);
+            Vic.RowSelect.Should().Be((value & 0x08) != 0);
+            Vic.YScroll.Should().Be(value & 0x07);
         }
 
         [Test]
@@ -156,19 +186,100 @@ namespace Breadbox.Test.Vic2
         [Test]
         public void SpriteEnable_StoreAndLoad([Random(1)] int value)
         {
+            TestSpriteRegister(0x15, value, Vic.SpriteEnabled);
+        }
+
+        [Test]
+        public void ControlRegister2_StoreAndLoad([Random(1)] int value)
+        {
             // Arrange
-            var expectedValue = value & 0xFF;
-            var expectedBits = Enumerable.Range(0, 8).Select(i => (value & (1 << i)) != 0).ToArray();
-            const int register = 0x15;
+            const int register = 0x16;
+            var expectedValue = (value & 0x3F) | 0xC0;
 
             // Act
             Vic.WriteRegister(register, value);
 
             // Assert
-            Vic.ReadRegister(register).Should().Be(expectedValue);
-            var observedBits = Enumerable.Range(0, 8).Select(Vic.SpriteEnabled);
-            observedBits.ShouldAllBeEquivalentTo(expectedBits);
+            var observedValue = Vic.ReadRegister(register);
+            observedValue.Should().Be(expectedValue);
+            Vic.Res.Should().Be((value & 0x20) != 0);
+            Vic.MultiColorMode.Should().Be((value & 0x10) != 0);
+            Vic.ColumnSelect.Should().Be((value & 0x08) != 0);
+            Vic.XScroll.Should().Be(value & 0x07);
         }
+
+        [Test]
+        public void SpriteYExpansion_StoreAndLoad([Random(1)] int value)
+        {
+            TestSpriteRegister(0x17, value, Vic.SpriteYExpansionEnabled);
+        }
+
+        [Test]
+        public void SpriteDataPriority_StoreAndLoad([Random(1)] int value)
+        {
+            TestSpriteRegister(0x1B, value, Vic.SpriteDataPriority);
+        }
+
+        [Test]
+        public void SpriteMultiColorEnable_StoreAndLoad([Random(1)] int value)
+        {
+            TestSpriteRegister(0x1C, value, Vic.SpriteMultiColorEnabled);
+        }
+
+        [Test]
+        public void SpriteXExpansion_StoreAndLoad([Random(1)] int value)
+        {
+            TestSpriteRegister(0x1D, value, Vic.SpriteXExpansionEnabled);
+        }
+
+        [Test]
+        public void SpriteBorderColor_StoreAndLoad([Random(1)] int value)
+        {
+            TestColorRegister(0x20, value);
+        }
+
+        [Test]
+        public void BackgroundColor0_StoreAndLoad([Random(1)] int value)
+        {
+            TestColorRegister(0x21, value);
+        }
+
+        [Test]
+        public void BackgroundColor1_StoreAndLoad([Random(1)] int value)
+        {
+            TestColorRegister(0x22, value);
+        }
+
+        [Test]
+        public void BackgroundColor2_StoreAndLoad([Random(1)] int value)
+        {
+            TestColorRegister(0x23, value);
+        }
+
+        [Test]
+        public void BackgroundColor3_StoreAndLoad([Random(1)] int value)
+        {
+            TestColorRegister(0x24, value);
+        }
+
+        [Test]
+        public void SpriteMultiColor0_StoreAndLoad([Random(1)] int value)
+        {
+            TestColorRegister(0x25, value);
+        }
+
+        [Test]
+        public void SpriteMultiColor1_StoreAndLoad([Random(1)] int value)
+        {
+            TestColorRegister(0x26, value);
+        }
+
+        [Test]
+        public void SpriteColors_StoreAndLoad([Range(0x27, 0x2E)] int register, [Random(1)] int value)
+        {
+            TestColorRegister(register, value);
+        }
+
 
     }
 }
