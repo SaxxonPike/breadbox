821b35fa9c76172fc0c9fc7bb9f74e26cf8adca4
Anthony Konzel <saxxonpike@gmail.com>
2016-06-21 09:53:27 -0500

    Simplify PLA matching.

--- BreadboxF/System.Commodore64SystemPla.fs
+++ BreadboxF/System.Commodore64SystemPla.fs
@@ -55,85 +55,93 @@ type Commodore64SystemPla (config:Commodore64SystemPlaConfiguration) =
             | 0x1000, _, false -> char
             | _ -> ram
 
-    let getMode () =
-        match readLoRam(), readHiRam(), readGame(), readExRom() with
-            | false, false, false, false -> 0xb0000
-            | false, false, false, true -> 0xb0001
-            | false, false, true, false -> 0xb0010
-            | false, false, true, true -> 0xb0011
-            | false, true, false, false -> 0xb0100
-            | false, true, false, true -> 0xb0101
-            | false, true, true, false -> 0xb0110
-            | false, true, true, true -> 0xb0111
-            | true, false, false, false -> 0xb1000
-            | true, false, false, true -> 0xb1001
-            | true, false, true, false -> 0xb1010
-            | true, false, true, true -> 0xb1011
-            | true, true, false, false -> 0xb1100
-            | true, true, false, true -> 0xb1101
-            | true, true, true, false -> 0xb1110
-            | _ -> 0xb1111
+    let mode1111Read address =
+        match address >>> 12 with
+            | 0xA | 0xB -> basic
+            | 0xD -> if readCharen() then io else char
+            | 0xE | 0xF -> kernal
+            | _ -> ram
+
+    let mode011xRead address =
+        match address >>> 12 with
+            | 0xD -> if readCharen() then io else char
+            | 0xE | 0xF -> kernal
+            | _ -> ram
+
+    let mode1000Read address =
+        if (address >>> 12 = 0xD && readCharen()) then io else ram
+
+    let mode101xRead address =
+        if (address >>> 12 = 0xD && readCharen()) then io else char
+
+    let mode1100Read address =
+        match address >>> 12 with
+            | 0x8 | 0x9 -> romL
+            | 0xA | 0xB -> romH
+            | 0xD -> if readCharen() then io else char
+            | 0xE | 0xF -> kernal
+            | _ -> ram
+
+    let mode0100Read address =
+        match address >>> 12 with
+            | 0xA | 0xB -> romH
+            | 0xD -> if readCharen() then io else char
+            | 0xE | 0xF -> kernal
+            | _ -> ram
+
+    let mode1110Read address =
+        match address >>> 12 with
+            | 0x8 | 0x9 -> romL
+            | 0xA | 0xB -> basic
+            | 0xD -> if readCharen() then io else char
+            | 0xE | 0xF -> kernal
+            | _ -> ram
+
+    let modexx01Read address =
+        match address >>> 12 with
+            | 0x0 -> ram
+            | 0x8 | 0x9 -> romL
+            | 0xD -> io
+            | 0xE | 0xF -> romH
+            | _ -> none
+
+    let defaultRead _ =
+        ram
+
+    let modexx01Write address =
+        match address >>> 12 with
+            | 0x0 -> ram
+            | 0x8 | 0x9 -> romL
+            | 0xD -> io
+            | 0xE | 0xF -> romH
+            | _ -> none
+
+    let mode001xWrite address =
+        ram
+
+    let defaultWrite address =
+        if (address >>> 12 = 0xD && readCharen()) then io else ram
 
     let readTarget address =
-        match getMode() with
-            | 0b1111 ->
-                match address >>> 12 with
-                    | 0xA | 0xB -> basic
-                    | 0xD -> if readCharen() then io else char
-                    | 0xE | 0xF -> kernal
-                    | _ -> ram
-            | 0b0110 | 0b0111 ->
-                match address >>> 12 with
-                    | 0xD -> if readCharen() then io else char
-                    | 0xE | 0xF -> kernal
-                    | _ -> ram
-            | 0b1000 ->
-                if (address >>> 12 = 0xD && readCharen()) then io else ram
-            | 0b1010 | 0b1011 ->
-                if (address >>> 12 = 0xD && readCharen()) then io else char
-            | 0b0000 | 0b0010 | 0b0011 ->
-                ram
-            | 0b1100 ->
-                match address >>> 12 with
-                    | 0x8 | 0x9 -> romL
-                    | 0xA | 0xB -> romH
-                    | 0xD -> if readCharen() then io else char
-                    | 0xE | 0xF -> kernal
-                    | _ -> ram
-            | 0b0100 ->
-                match address >>> 12 with
-                    | 0xA | 0xB -> romH
-                    | 0xD -> if readCharen() then io else char
-                    | 0xE | 0xF -> kernal
-                    | _ -> ram
-            | 0b1110 ->
-                match address >>> 12 with
-                    | 0x8 | 0x9 -> romL
-                    | 0xA | 0xB -> basic
-                    | 0xD -> if readCharen() then io else char
-                    | 0xE | 0xF -> kernal
-                    | _ -> ram
-            | _ ->
-                match address >>> 12 with
-                    | 0x0 -> ram
-                    | 0x8 | 0x9 -> romL
-                    | 0xD -> io
-                    | 0xE | 0xF -> romH
-                    | _ -> none
+        address |>
+            match readLoRam(), readHiRam(), readGame(), readExRom() with
+                | _, _, false, true -> modexx01Read
+                | true, false, true, _ -> mode101xRead
+                | false, true, true, _ -> mode011xRead
+                | true, true, true, true -> mode1111Read
+                | true, true, true, false -> mode1110Read
+                | true, true, false, false -> mode1100Read
+                | true, false, false, false -> mode1000Read
+                | false, true, false, false -> mode0100Read
+                | _ -> defaultRead
                     
     let writeTarget address =
-        match getMode() with
-            | 0b0000 | 0b0010 | 0b0011 ->
-                ram
-            | 0b0001 | 0b0101 | 0b1001 | 0b1101 ->
-                match address >>> 12 with
-                    | 0x0 -> ram
-                    | 0x8 | 0x9 -> romL
-                    | 0xD -> io
-                    | 0xE | 0xF -> romH
-                    | _ -> none
-            | _ ->
-                if (address >>> 12 = 0xD && readCharen()) then io else ram
+        address |>
+            match readLoRam(), readHiRam(), readGame(), readExRom() with
+                | false, false, true, _ -> mode001xWrite
+                | false, false, false, false -> mode001xWrite
+                | _, _, false, true -> modexx01Write
+                | _ -> defaultWrite
     
     let vicRead (address) = vicReadTarget(address).Read(address)
     let vicPeek (address) = vicReadTarget(address).Peek(address)
